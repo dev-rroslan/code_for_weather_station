66ecfea22637fcb251d16f7e0742e1d3bfe6a97c
dev-rroslan <user.email>
2023-09-05 10:21:29 +0800

    initial commit

---
+++ weather_tracker/lib/weather_tracker_web/controllers/weather_conditions_controller.ex
@@ -0,0 +1,28 @@
+defmodule WeatherTrackerWeb.WeatherConditionsController do
+    use WeatherTrackerWeb, :controller
+  
+    require Logger
+  
+    alias WeatherTracker.{
+      WeatherConditions,
+      WeatherConditions.WeatherCondition
+    }
+  
+    def create(conn, params) do
+      case WeatherConditions.create_entry(params) do
+        {:ok, weather_condition = %WeatherCondition{}} ->
+          Logger.debug("Successfully created a weather condition entry")
+  
+          conn
+          |> put_status(:created)
+          |> json(weather_condition)
+  
+        error ->
+          Logger.warn("Failed to create a weather entry: #{inspect(error)}")
+  
+          conn
+          |> put_status(:unprocessable_entity)
+          |> json(%{message: "Poorly formatted payload"})
+      end
+    end
+  end
